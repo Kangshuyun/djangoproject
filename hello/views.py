555da6b5fcbc4ccefa324beeb0a3be63b917ff9d
Kangshuyun <157193624+Kangshuyun@users.noreply.github.com>
2024-03-14 11:31:55 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("helloworldeveryone")
+    return render (request, "hello/index.html")
 
 
-def index(request,name):
-    return render(request, "hello/index.html" ,
+def greet(request,name):
+    return render(request, "hello/greet.html" ,
                   {
                       "name" : name.capitalize()
                   })
