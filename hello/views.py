224c9363999b3f4c621481952e0b587e4e6e0582
Kangshuyun <157193624+Kangshuyun@users.noreply.github.com>
2024-03-14 11:23:22 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -5,12 +5,6 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse("helloworldeveryone")
 
-def error_404(request, exception):
-    print(hh)
-    return render(request, 'templates/505_404.html', status=404)
-
-def error_500(request):
-    return render(request, 'templates/505_404.html', status=500)
 
 def greet(request,name):
     return render(request, "hello/greet.html" ,
